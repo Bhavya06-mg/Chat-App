519f4f2a7ad633333115b9b02ce9dafc6118a649
Bhavya <bhavyamadev681@gmail.com>
2025-08-07 22:19:59 +0530

    Initial commit

---
+++ backend/routes/chatRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const { chats } = require("../data/data");
+const { protect } = require("../middleware/authMiddleware");
+const{accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup} = require ("../controllers/chatController")
+
+router.route("/").post(protect,accessChat);
+router.route("/").get(protect, fetchChats);
+router.route("/group").post(protect, createGroupChat);
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupadd").put(protect, addToGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
+
+
+
+module.exports = router;
