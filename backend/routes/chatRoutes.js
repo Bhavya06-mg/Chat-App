461c8754738c03cac2c92433c7f56142c81f1f0e
Bhavya <bhavyamadev681@gmail.com>
2025-08-09 23:07:59 +0530

    Updated frontend and backend code

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 });
 
 router.route("/").post(protect, accessChat);
-router.route("/chats").get(protect, fetchChats);
+router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
 router.route("/rename").put(protect, renameGroup);
 router.route("/groupremove").put(protect, removeFromGroup);
